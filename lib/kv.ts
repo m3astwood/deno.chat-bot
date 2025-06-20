9bd59afe5aa0a344d0ecdb83ac5a9a423bfab18e
Max Franklin <max.franklin@arturia.com>
2025-06-20 16:49:30 +0200

    Update listing for saved members

--- lib/kv.ts
+++ lib/kv.ts
@@ -14,6 +14,6 @@ export async function writeMembers(members: SpaceMember[]) {
   }
 }
 
-export async function getSavedMembers() {
-  return await KV.get(['members'])
+export function getSavedMembers() {
+  return KV.list({ prefix: ['members'] })
 }
