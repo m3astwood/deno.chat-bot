5c50e76a890e99e2e6ed6e72b3948b0e7a68b954
Max Franklin <max.franklin@arturia.com>
2025-06-25 11:08:54 +0200

    Add console to see data structure

--- lib/kv.ts
+++ lib/kv.ts
@@ -3,9 +3,17 @@ import { SpaceMember } from '../types/Data.ts'
 const KV = await Deno.openKv()
 
 export async function writeMembers(members: SpaceMember[]) {
+  // first get existing members
+  const { value: existing } = await getSavedMembers()
+
+  // loop through existing members and passed members to add non existent
+  console.log(existing)
+
+  // write all members to KV again
+
   await KV.set(['members'], members.map(m => ({ ...members, breakfasts: 0 })))
 }
 
-export function getSavedMembers() {
-  return KV.list({ prefix: ['members'] })
+export async function getSavedMembers() {
+  return await KV.get(['members'])
 }
