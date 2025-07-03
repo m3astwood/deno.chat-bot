d38afbb5400475b9b8aca94a80b93373504ed5c1
Max Franklin <max.franklin@arturia.com>
2025-07-03 16:54:21 +0200

    Fix lingering trycatch add return data to reset and cancel

--- controllers/cardCommands.ts
+++ controllers/cardCommands.ts
@@ -5,54 +5,60 @@ import { CardCommandCode, OnCommand } from '../types/Commands.ts'
 
 export const CardCommands: Map<string, OnCommand> = new Map()
 
+CardCommands.set(CardCommandCode.Cancel, {
+  execute: async (event, params) => {
+    return { text: `CANCEL ${params}!!!!` }
+  },
+})
+
 CardCommands.set(CardCommandCode.Reset, {
-  execute: async (spaceName: string) => {},
+  execute: async (event, params) => {
+    return { text: 'RESET!!!!' }
+  },
 })
 
 CardCommands.set(CardCommandCode.Choose, {
   execute: async (event) => {
-    try {
-      const { members: {stringInputs: { value: toExclude } } } = event.common.formInputs
+    const { members: { stringInputs: { value: toExclude } } } = event.common.formInputs
 
-      // {
-      //   members: {
-      //     stringInputs: {
-      //       value: [ "users/114134330763224107043", "users/103965345231025561001" ]
-      //     }
-      //   }
-      // }
-      const spaceName = event.space.name
-      console.log('Getting members for space:', spaceName)
-      const members = await getMembers(spaceName)
+    // {
+    //   members: {
+    //     stringInputs: {
+    //       value: [ "users/114134330763224107043", "users/103965345231025561001" ]
+    //     }
+    //   }
+    // }
+    const spaceName = event.space.name
+    console.log('Getting members for space:', spaceName)
+    const members = await getMembers(spaceName)
 
-      const consolidatedMembers = await consolidateMembers(spaceName, members)
-      console.log('consolidated', consolidatedMembers)
+    const consolidatedMembers = await consolidateMembers(spaceName, members)
+    console.log('consolidated', consolidatedMembers)
 
-      const { chosen, error } = chooseTwoMembers(consolidatedMembers, toExclude)
-      if (error) {
-        return {
-          privateMessageViewer: {
-            name: event.user.name
-          },
-          text: error
-        }
+    const { chosen, error } = chooseTwoMembers(consolidatedMembers, toExclude)
+    if (error) {
+      return {
+        privateMessageViewer: {
+          name: event.user.name,
+        },
+        text: error,
       }
-
-      if (!chosen) {
-        throw Error('Something has gone wrong with breakfast selection')
-      }
-
-      const [ personOne, personTwo ] = chosen
-      // update chosen members breakfasts
-      const updatedMembers = updateMembers(consolidatedMembers, personOne, personTwo)
-
-      console.log('updated', updatedMembers)
-
-      // write members to KV
-      await writeMembers(spaceName, updatedMembers)
-
-      // return message
-      return { text: `Bonjour, semaine prochaine le petit dej est fourni par : <${personOne.name}> et <${personTwo.name}>!` }
     }
+
+    if (!chosen) {
+      throw Error('Something has gone wrong with breakfast selection')
+    }
+
+    const [personOne, personTwo] = chosen
+    // update chosen members breakfasts
+    const updatedMembers = updateMembers(consolidatedMembers, personOne, personTwo)
+
+    console.log('updated', updatedMembers)
+
+    // write members to KV
+    await writeMembers(spaceName, updatedMembers)
+
+    // return message
+    return { text: `Bonjour, semaine prochaine le petit dej est fourni par : <${personOne.name}> et <${personTwo.name}>!` }
   },
 })
