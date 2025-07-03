13d76ec1da4f89a82272a764535fd310594f1cf0
Max Franklin <max.franklin@arturia.com>
2025-07-03 16:01:46 +0200

    Scaffold Choose command

--- controllers/cardCommands.ts
+++ controllers/cardCommands.ts
@@ -2,10 +2,14 @@ import { CardCommandCode, OnCommand } from '../types/Commands.ts'
 
 export const CardCommands: Map<string, OnCommand> = new Map()
 
-CardCommands.set(CardCommandCode.Reset, (spaceName: string) => {
-
+CardCommands.set(CardCommandCode.Reset, {
+  execute: async (spaceName: string) => {},
 })
 
-CardCommands.set(CardCommandCode.Reset, () => {
+CardCommands.set(CardCommandCode.Choose, {
+  execute: async (users) => {
+    console.log(users)
 
+    return users
+  },
 })
